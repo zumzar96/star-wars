45ae4dc4388ff551296b8c3164700da5337d4ac3
ismail <ismailgim96@gmail.com>
2022-01-12 14:06:23 +0100

    Adding useCallback and useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -8,7 +8,7 @@ function App() {
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
 
-    async function fetchMoviesHandler() {
+    const fetchMoviesHandler = useCallback(async () => {
         setIsLoading(true);
         setError(null);
         try {
@@ -32,9 +32,12 @@ function App() {
             setError(error.message);
         }
         setIsLoading(false);
-    };
+    }, []);
 
-    
+    useEffect(() => {
+        fetchMoviesHandler();
+    }, [fetchMoviesHandler]);
+
     let content = <p>Found no movies.</p>;
 
     if (movies.length > 0) {
@@ -60,3 +63,4 @@ function App() {
 }
 
 export default App;
+
